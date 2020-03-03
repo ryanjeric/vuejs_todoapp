9b19ca8a8a8be91367b4c58c8832dbdd34e196b9
ryanjericsabado <ryanjericsabado@yahoo.com>
2020-03-03 17:51:26 +0800

    INIT

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p> 
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Hic eveniet asperiores esse nemo natus quod,
+        iure doloribus distinctio delectus rerum veritatis nulla 
+        veniam error cumque quae cupiditate quasi minus laborum.
+    </p>
+  </div>
+</template>
